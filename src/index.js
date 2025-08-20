84c4e17d87cb20a355dfee7b2b59fa4a3303b1d6
matthewsays23 <jason.jones06@outlook.com>
2025-08-20 00:40:13 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const {
   Collection 
 } = require("discord.js");
 
+
+
 const fs = require("fs");
 require("dotenv").config();
 
@@ -42,4 +44,19 @@ const commandFolders = fs.readdirSync("./src/commands");
   }
 
   client.login(token);
+
+  
+// after client.login(...)
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const webhookRouter = require("../bot/webhook")(client);
+app.use("/webhook", webhookRouter);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Bot webhook listening on ${port}`));
+
+
 })();
+
